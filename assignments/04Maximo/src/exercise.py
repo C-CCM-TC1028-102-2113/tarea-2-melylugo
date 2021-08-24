a6b3bd028fee4b411a990cf2883726ec24431842
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-24 00:11:30 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -4,14 +4,25 @@ def main():
     num2 = int(input("Ingresa el segundo número: "))
     num3 = int(input("Ingresa el tercer número: "))
 
-    if num1>num2 and num1>num3:
-        print (num1)
-    elif num2>num1 and num2>num3:
-        print (num2)
-    elif num3>num1 and num3>num2:
+    if num1<=num2 and num2<=num3:
         print (num3)
-    pass
 
+    elif num1<=num3 and num3<=num2:
+        print (num2)  
+
+    elif num2<=num1 and num1<=num3:
+        print (num3)
+
+    elif num2<=num3 and num3<=num1:
+        print (num1)
+
+    elif num3<=num1 and num1<=num2:
+        print (num2) 
+
+    elif num3<=num2 and num2<=num1:
+    print (num1) 
+    
+    pass
 
 if __name__=='__main__':
     main()
